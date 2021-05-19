4aae7217afc5581a3fbcb8d4705ca57d0686ef93
Xi Zhang <49449649+xzhangfox@users.noreply.github.com>
2021-05-19 16:26:08 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,21 @@ def home():
 #To use the predict button in our web-app
 @app.route('/predict',methods=['POST'])
 def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+    int_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(int_features)]
+    prediction = model.predict(final_features)
+
+    output = {'predicted': [prediction[0]]}
+
+    return render_template('index.html', prediction_text=output)
+
+@app.route('/processjson', methods = ['POST'])
+def processjson():
     data = request.get_json()
+
     # pull the inputs
     enginesize = data['enginesize']
     cylinders = data['cylinders']
@@ -26,9 +40,10 @@ def predict():
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
-    output = jsonify({'enginesize': enginesize, 'cylinders': cylinders, 'fuel':fuel, 'prediction': int(prediction)})
+    
+    return jsonify({'result' : 'Success!', 'enginesize': enginesize, 'cylinders': cylinders, 'fuel':fuel, 'prediction': int(prediction)})
 
-    return output
+
 
 if __name__ == "__main__":
     app.run(debug=True)
